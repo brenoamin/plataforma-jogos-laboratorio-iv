a1a8114e90e33d46a1f37f6a3e5a13f9d122c45f
RF-Vinicius <reisf.vinicius@gmail.com>
2023-11-19 21:00:55 -0300

    versao para 128bits

--- modulo_merge/merge.v
+++ modulo_merge/merge.v
@@ -1,9 +1,9 @@
-module merge (R_bg, G_bg, B_bg, R_sp, G_sp, B_sp, R_out, G_out, B_out, posX_bg, posY_bg, posX_sp, posY_sp, collision, reset, clk);
+module merge (R_bg, G_bg, B_bg, R_sp, G_sp, B_sp, R_outRegA, G_outRegA, B_outRegA, R_outRegB, G_outRegB, B_outRegB, posX_bg, posY_bg, posX_sp, posY_sp, collision, reset, clk);
 
-    input wire clk, reset;
+    input wire clk, reset, readVgaSelector;
     input wire [7:0] R_bg, G_bg, B_bg, R_sp, G_sp, B_sp;
     input wire [9:0] posX_bg, posY_bg, posX_sp, posY_sp;
-    output reg [7:0] R_out, G_out, B_out;
+    output reg [127:0] R_outRegA, G_outRegA, B_outRegA, R_outRegB, G_outRegB, B_outRegB;
     output reg [3:0] collision;
 
     localparam SPRITE_SIZE = 16;
@@ -13,51 +13,90 @@ module merge (R_bg, G_bg, B_bg, R_sp, G_sp, B_sp, R_out, G_out, B_out, posX_bg, 
                G_trans = 8'h17,
                B_trans = 8'h17;
 
-					
+	
+
+    // Quando o VGA começar a ler o registrado A, zera o B. Assim como, quando começa a ler o B, zera o A
+    always @(posedge readVgaSelector)
+    begin
+        R_outRegA <= 0;
+        G_outRegA <= 0;
+        B_outRegA <= 0;
+    end
+
+    always @(negedge readVgaSelector)
+    begin
+        R_outRegB <= 0;
+        G_outRegB <= 0;
+        B_outRegB <= 0;
+    end
+
+
     always@(posedge clk) 
     begin
         if (reset)
         begin
-            R_out <= 8'h00;
-            G_out <= 8'h00;
-            B_out <= 8'h00;
+            R_outRegA <= 0;
+            G_outRegA <= 0;
+            B_outRegA <= 0;
+            R_outRegB <= 0;
+            G_outRegB <= 0;
+            B_outRegB <= 0;
+            
             collision <= 1'b0;
         end
         else
         begin
-            if (R_sp == R_trans && G_sp == G_trans && B_sp == B_trans) //Verifica se a entrada do sprit é a cor transparente naquele pixel
+
+            if (readVgaSelector) //Se o VGA está lendo o registrador tipo B prepara os pixels do registrador A,
             begin
-                R_out <= R_bg;
-                G_out <= G_bg;
-                B_out <= B_bg;
+                if (R_sp == R_trans && G_sp == G_trans && B_sp == B_trans) //Verifica se a entrada do sprit é a cor transparente naquele pixel, se for preenche com o background
+                begin 
+                    R_outRegA <= {R_outRegA, R_bg};
+                    G_outRegA <= {G_outRegA, G_bg};
+                    B_outRegA <= {B_outRegA, B_bg};
+                end
+                else begin
+                    R_outRegA <= {R_outRegA, R_sp};
+                    G_outRegA <= {G_outRegA, G_sp};
+                    B_outRegA <= {B_outRegA, B_sp};
+                end
+            end else
+                if (R_sp == R_trans && G_sp == G_trans && B_sp == B_trans) //Verifica se a entrada do sprit é a cor transparente naquele pixel, se for preenche com o background
+                begin 
+                    R_outRegB <= {R_outRegB, R_bg};
+                    G_outRegB <= {G_outRegB, G_bg};
+                    B_outRegB <= {B_outRegB, B_bg};
+                end
+                else begin
+                    R_outRegB <= {R_outRegB, R_sp};
+                    G_outRegB <= {G_outRegB, G_sp};
+                    B_outRegB <= {B_outRegB, B_sp};
+                end
+         
+        
+
+            if (posX_sp + SPRITE_SIZE >= BG_SIZE_X)
+            begin
+                collision <= 4'b0001; // Colisão à direita
+            end
+            else if (posX_sp <= 0)
+            begin
+                collision <= 4'b0010; // Colisão à esquerda
+            end
+            else if (posY_sp + SPRITE_SIZE >= BG_SIZE_Y)
+            begin
+                collision <= 4'b0100; // Colisão em baixo
+            end
+            else if (posY_sp <= 0)
+            begin
+                collision <= 4'b1000; // Colisão em cima
             end
             else
             begin
-                R_out <= R_sp;
-                G_out <= G_sp;
-                B_out <= B_sp;
-            end          
-        end
-
-        if (posX_sp + SPRITE_SIZE >= BG_SIZE_X)
-        begin
-            collision <= 4'b0001; // Colisão à direita
-        end
-        else if (posX_sp <= 0)
-        begin
-            collision <= 4'b0010; // Colisão à esquerda
-        end
-        else if (posY_sp + SPRITE_SIZE >= BG_SIZE_Y)
-        begin
-            collision <= 4'b0100; // Colisão em baixo
-        end
-        else if (posY_sp <= 0)
-        begin
-            collision <= 4'b1000; // Colisão em cima
-        end
-        else
-        begin
-            collision <= 4'b0000; // Sem colisão
+                collision <= 4'b0000; // Sem colisão
+            end
         end
     end
+
+
 endmodule
--- modulo_merge/merge_tb.v
+++ modulo_merge/merge_tb.v
@@ -1,38 +1,37 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ns // Definindo a escala de tempo para simulação
 
-module merge_tb;
+module testbench;
 
-    // Sinais de clock e reset
-    reg clk;
-    reg reset;
+  // Definindo parâmetros
+  reg clk, reset, readVgaSelector;
+  reg [7:0] R_bg, G_bg, B_bg, R_sp, G_sp, B_sp;
+  reg [9:0] posX_bg, posY_bg, posX_sp, posY_sp;
+  wire [127:0] R_outRegA, G_outRegA, B_outRegA, R_outRegB, G_outRegB, B_outRegB;
+  wire [3:0] collision;
 
-    // Sinais de entrada
-    reg [7:0] R_bg, G_bg, B_bg, R_sp, G_sp, B_sp;
-    reg [9:0] posX_bg, posY_bg, posX_sp, posY_sp;
-
-    // Sinais de saída
-    wire [7:0] R_out, G_out, B_out;
-    wire [3:0] collision;
-
-    // Instanciação do módulo a ser testado
-    merge uut (
-        .clk(clk),
-        .reset(reset),
-        .R_bg(R_bg),
-        .G_bg(G_bg),
-        .B_bg(B_bg),
-        .R_sp(R_sp),
-        .G_sp(G_sp),
-        .B_sp(B_sp),
-        .R_out(R_out),
-        .G_out(G_out),
-        .B_out(B_out),
-        .posX_bg(posX_bg),
-        .posY_bg(posY_bg),
-        .posX_sp(posX_sp),
-        .posY_sp(posY_sp),
-        .collision(collision)
-    );
+  // Instanciando o módulo a ser testado
+  merge uut (
+    .clk(clk),
+    .reset(reset),
+    .readVgaSelector(readVgaSelector),
+    .R_bg(R_bg),
+    .G_bg(G_bg),
+    .B_bg(B_bg),
+    .R_sp(R_sp),
+    .G_sp(G_sp),
+    .B_sp(B_sp),
+    .R_outRegA(R_outRegA),
+    .G_outRegA(G_outRegA),
+    .B_outRegA(B_outRegA),
+    .R_outRegB(R_outRegB),
+    .G_outRegB(G_outRegB),
+    .B_outRegB(B_outRegB),
+    .posX_bg(posX_bg),
+    .posY_bg(posY_bg),
+    .posX_sp(posX_sp),
+    .posY_sp(posY_sp),
+    .collision(collision)
+  );
 
 
     initial begin
@@ -43,52 +42,438 @@ module merge_tb;
 
     initial begin
     reset = 1'b1;
-        #10;
+        #2;
     reset = 1'b0;
+    readVgaSelector = 1'b1
+
     end
 
     initial 
         begin
+            //A cada pulso de clock preenche os registradores A
+            #1;
             R_bg = 8'h20;
             G_bg = 8'h50;
             B_bg = 8'h40;
-            posX_bg = 600;
-            posY_bg = 500;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
 
             R_sp = 8'h17;
             G_sp = 8'h17;
             B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
 
-            posX_sp = 1;
-            posY_sp = 1;
-
-            #20;
-
+            #1;
             R_bg = 8'h20;
             G_bg = 8'h50;
             B_bg = 8'h40;
-            posX_bg = 600;
-            posY_bg = 500;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
 
             R_sp = 8'h30;
             G_sp = 8'h54;
             B_sp = 8'h41;
-            posX_sp = ;
-            posY_sp = 1;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
 
-            #20;
-
+            #1;
             R_bg = 8'h20;
             G_bg = 8'h50;
             B_bg = 8'h40;
-            posX_bg = 990;
-            posY_bg = 500;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
 
             R_sp = 8'h30;
             G_sp = 8'h54;
             B_sp = 8'h41;
-            posX_sp = 1;
-            posY_sp = 1;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h17;
+            G_sp = 8'h17;
+            B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h17;
+            G_sp = 8'h17;
+            B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h17;
+            G_sp = 8'h17;
+            B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+
+            //Seleciona o registrador A como leitura
+            #5;
+            readVgaSelector = 1'b0;
+            5#;
+
+
+            //A cada pulso de clock preenche os registradores B
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h17;
+            G_sp = 8'h17;
+            B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h17;
+            G_sp = 8'h17;
+            B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h17;
+            G_sp = 8'h17;
+            B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b0111110100;//500
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h17;
+            G_sp = 8'h17;
+            B_sp = 8'h17;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b0111110100;//500
+
+            R_sp = 8'h87;
+            G_sp = 8'h32;
+            B_sp = 8'h50;
+            posX_sp = 10'b0000000001;
+            posY_sp = 10'b1111011110;//990
+
+            #1;
+            R_bg = 8'h20;
+            G_bg = 8'h50;
+            B_bg = 8'h40;
+            posX_bg = 10'b1111011110;//990
+            posY_bg = 10'b1111011110;//990
+
+            R_sp = 8'h30;
+            G_sp = 8'h54;
+            B_sp = 8'h41;
+            posX_sp = 10'b1111011110;//990
+            posY_sp = 10'b0000000001;
         end
 
 endmodule
