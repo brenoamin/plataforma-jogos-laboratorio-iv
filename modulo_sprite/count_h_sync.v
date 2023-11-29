ba22298d12cdaa500ee2b49e6fb01e8ebfbf66da
Elber Cerqueira <32513208+elbervitor@users.noreply.github.com>
2023-11-28 23:16:13 -0300

    Nova compilação de modulo sprite
    Alteração massiva de código. Módulos baseados em FSM simplificados. Necessario mudancas temporais e TB.

---
+++ modulo_sprite/count_h_sync.v
@@ -0,0 +1,25 @@
+module count_h_sync(
+    input wire clk,
+    input wire rst,
+    input wire h_sync,
+	 input wire v_sync,
+    output wire [9:0] V_pos_in
+);
+
+
+    always @(posedge h_sync) begin
+        V_pos_in <= V_pos_in + 1;
+    end
+
+    always @(posedge v_sync) begin
+        V_pos_in <= 0;
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            V_pos_in <= 0;
+        end
+    end
+
+
+endmodule
